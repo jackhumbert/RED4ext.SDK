4209024f57c4215c03fb5dc6081141aa4e776f55
Jack Humbert <jack.humb@gmail.com>
2025-06-17 09:47:00 -0400

    remove address incl

--- include/RED4ext/Scripting/Natives/physicsProxyManager-inl.hpp
+++ include/RED4ext/Scripting/Natives/physicsProxyManager-inl.hpp
@@ -4,7 +4,6 @@
 #include <RED4ext/Scripting/Natives/physicsProxyManager.hpp>
 #endif
 
-#include "Addresses.hpp"
 #include <RED4ext/Relocation.hpp>
 
 RED4ext::physics::ProxyManager * RED4ext::physics::proxyManager = *reinterpret_cast<RED4ext::physics::ProxyManager **>(reinterpret_cast<uintptr_t>(GetModuleHandle(nullptr)) + physics_proxyManager_Addr);
