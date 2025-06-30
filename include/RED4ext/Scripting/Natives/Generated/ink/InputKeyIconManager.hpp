79316797be2f965d72b2eebe4d658726646ead85
Jack Humbert <jack.humb@gmail.com>
2025-06-30 17:01:00 -0400

    icon manager stuff

--- include/RED4ext/Scripting/Natives/Generated/ink/InputKeyIconManager.hpp
+++ include/RED4ext/Scripting/Natives/Generated/ink/InputKeyIconManager.hpp
@@ -7,17 +7,38 @@
 #include <cstdint>
 #include <RED4ext/Common.hpp>
 #include <RED4ext/ISerializable.hpp>
+#include <RED4ext/Scripting/Natives/Generated/ink/TextureAtlas.hpp>
+#include <RED4ext/Scripting/Natives/Generated/input/EInputDevice.hpp>
 
 namespace RED4ext
 {
 namespace ink
 {
+    
+struct InputIconPartData {
+    CName part;
+};
+
+struct InputIconTextureData {
+    uint64_t unk00;
+    uint64_t unk08;
+    ResourcePath atlasPath;
+    CName partName;
+};
+
 struct InputKeyIconManager : ISerializable
 {
     static constexpr const char* NAME = "inkInputKeyIconManager";
     static constexpr const char* ALIAS = NAME;
 
-    uint8_t unk30[0x1E8 - 0x30]; // 30
+    struct InputDeviceTextureData {
+        Handle<ink::TextureAtlas> atlas;
+        Map<uint32_t, ink::InputIconPartData> parts;
+    };
+
+    uint8_t unk30[0x68 - 0x30]; // 30
+    Map<input::EInputDevice, InputDeviceTextureData> inputDevices; // 68
+    uint8_t unk90[0x1E8 - 0x90]; // 90
 };
 RED4EXT_ASSERT_SIZE(InputKeyIconManager, 0x1E8);
 } // namespace ink
